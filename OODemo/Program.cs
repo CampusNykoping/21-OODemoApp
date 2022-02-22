f28621577b5590702619d7bdeaf07b5448f1fdab
Claes Engelin <claes.engelin@zocom.se>
2022-02-22 17:17:42 +0100

    Add project files.

---
+++ OODemo/Program.cs
@@ -0,0 +1,38 @@
+﻿
+var dog = new Dog(5, "Collie");
+dog.Sleep();
+dog.WagTail();
+
+//Console.WriteLine("Press any key");
+//Console.ReadKey();
+
+public class Mammal
+{
+    public int Age { get; set; }
+
+    public Mammal(int age)
+    {
+        this.Age = age;
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("Shhh! I'm sleeping!");
+    }
+}
+
+public class Dog : Mammal
+{
+    public string Breed { get; set; }
+
+    public Dog(int age, string breed)
+      : base(age)
+    {
+        this.Breed = breed;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Tail wagging...");
+    }
+}
---
+++ PolymorphismDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿
+class Animal  // Base class (parent) 
+{
+    public  void animalSound()
+    {
+        Console.WriteLine("The animal makes a sound");
+    }
+}
+
+class Pig : Animal  // Derived class (child) 
+{
+    public  void animalSound()
+    {
+        Console.WriteLine("The pig says: wee wee");
+    }
+}
+
+class Dog : Animal  // Derived class (child) 
+{
+    public  void animalSound()
+    {
+        Console.WriteLine("The dog says: bow wow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var myAnimal = new Animal();  // Create a Animal object
+        var myPig = new Pig();  // Create a Pig object
+        Animal myDog = new Dog();  // Create a Dog object
+        Dog myOtherDog = new Dog(); 
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
+        myOtherDog.animalSound();
+    }
+}
